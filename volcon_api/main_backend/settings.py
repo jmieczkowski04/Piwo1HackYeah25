f45cc67fdeaccdc74769f1135c4ff87825b4ec76
Awksi1 <awksi@proton.me>
2025-10-04 22:02:25 +0200

    Vulcon adding hosts

--- volcon_api/main_backend/settings.py
+++ volcon_api/main_backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-)6rkuq^$k3cl-6xp$19q^pihh738xx33g4nz-_3&)4ztxtw#vl
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
