5828ce6462eb3f1bfa329337a3aa89e4d9e3737c
kindaCacti <m.wawrzyniaczek@gmail.com>
2025-10-05 03:47:40 +0200

    added get set user coordinates

--- volcon_api/api/urls.py
+++ volcon_api/api/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from . import views
 from .views import attach_token_view
-from .user_requests import get_user_data, get_all_users
+from .user_requests import get_user_data, get_all_users, get_set_user_coordinates
 
 urlpatterns = [
     path('attach_token/', attach_token_view, name='attach_token'),
     path('user/', get_all_users, name='user_detail'),
     path('user/<int:id>/', get_user_data, name='user_detail'),
+    path('user/<int:id>/localization', get_set_user_coordinates, name='user_detail'),
 ]
