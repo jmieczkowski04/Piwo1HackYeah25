e4f447d03a6b98a31dd90781f3b04a675f758480
kindaCacti <m.wawrzyniaczek@gmail.com>
2025-10-05 11:40:34 +0200

    added file

---
+++ frontend/src/views/BrowserMapMain.vue
@@ -0,0 +1,16 @@
+<template>
+  <MapField :pins="pins" @pin="onPin" />
+</template>
+
+<script setup>
+import MapField from '@/components/browser/MapField.vue'
+import { mockGroupPins } from '@/mocks/mapPins.js'
+
+const pins = mockGroupPins()
+
+function onPin(p) {
+  // w kolejnej iteracji wstawimy to do lewego panelu wyników
+  console.log('Kliknięto pinezkę grupy:', p)
+}
+</script>
+
