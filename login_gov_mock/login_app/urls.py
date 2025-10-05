f842b581285b6484cfa0fa5442369e3bfecd2278
Jakub Mieczkowski <jmieczkowski04@gmail.com>
2025-10-05 09:51:31 +0200

    Pretty login.gov.pl

--- login_gov_mock/login_app/urls.py
+++ login_gov_mock/login_app/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from .views import login_view, get_id_by_pesel_view, register_view, register_page, login_page, user_data_by_id
 
 urlpatterns = [
+    path('', login_page, name='login_page'),
     path('login/', login_view, name='login'),
     path('get_id_by_pesel/', get_id_by_pesel_view, name='get_id_by_pesel'),
     path('register/', register_view, name='register'),
