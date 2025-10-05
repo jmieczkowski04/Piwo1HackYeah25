0ecafea99ce91831d454a356c2a68fab51311521
kindaCacti <m.wawrzyniaczek@gmail.com>
2025-10-05 10:49:13 +0200

    added new requirement to requirements.txt

--- volcon_api/api/views.py
+++ volcon_api/api/views.py
@@ -22,3 +22,38 @@ def attach_token_view(request):
 
 def check_token(request):
     print(request.session["access_token"])
+
+def search_event(request):
+    if request.method != 'POST':
+        return JsonResponse({"error": "unsupported request method"}, status=403)
+    
+    title = request.POST.get('title')
+    root_group_id = request.POST.get('root_group_id')
+    startdate = request.POST.get('startdate')
+    enddate = request.POST.get('enddate')
+    radius = request.POST.get('radius')
+    minimal_age = request.POST.get('minimalAge')
+    loc = request.POST.get('_loc')
+
+    filter = []
+    if root_group_id is not None:
+        filter.append(f"root_group_id:={root_group_id}")
+
+    if startdate is not None:
+        filter.append(f"startdate>={startdate}")
+
+    if enddate is not None:
+        filter.append(f"enddate<={enddate}")
+    
+    if radius is not None and loc is not None:
+        filter.append(f"location({loc[0]}, {loc[1]}, {radius})")
+    
+    if minimal_age is not None:
+        filter.append(f"minimal_age>={minimal_age}")
+
+    event_data = {
+        'q': title,
+        'query_by': 'title',
+        'filter_by': filter                                
+    }
+
